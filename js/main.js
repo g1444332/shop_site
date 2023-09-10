203f444a65d3cdb55810c6730a9a6b4abf4f8f17
g1444332 <sasamaksim10@gmail.com>
2023-09-10 15:25:51 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 function onButtonClick() {
     var filter = document.getElementById('filter_2');
+    var main = document.getElementById('main');
     if (filter.style.display == "flex") {
         filter.style.display = "none";
     }
     else {
+        var main = document.getElementById('main');
         filter.style.display = "flex";
     }
 }
