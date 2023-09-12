49a392a26e8c2d6fdb5a2780a5e7fc4de6f836cc
g1444332 <sasamaksim10@gmail.com>
2023-09-12 16:42:38 +0300

    update 2.0

--- js/main.js
+++ js/main.js
@@ -11,34 +11,60 @@ function onButtonClick() {
     }
 }
 
-function onButtonClick1(num) {
-    var list1 = [
-    document.getElementById('all'), 
-    document.getElementById('new'), 
-    document.getElementById('see'), 
-    document.getElementById('symku'),
-    document.getElementById('odayg'),
-    document.getElementById('sezon')
-    ];
-    var list2 = [
-    document.getElementById('all_menu'), 
-    document.getElementById('new_menu'), 
-    document.getElementById('see_menu'), 
-    document.getElementById('symku_menu'),
-    document.getElementById('odayg_menu'),
-    document.getElementById('sezon_menu')
-    ];
+// function onButtonClick1(num) {
+//     var list1 = [
+//     document.getElementById('all'), 
+//     document.getElementById('new'), 
+//     document.getElementById('see'), 
+//     document.getElementById('symku'),
+//     document.getElementById('odayg'),
+//     document.getElementById('sezon')
+//     ];
+//     var list2 = [
+//     document.getElementById('all_menu'), 
+//     document.getElementById('new_menu'), 
+//     document.getElementById('see_menu'), 
+//     document.getElementById('symku_menu'),
+//     document.getElementById('odayg_menu'),
+//     document.getElementById('sezon_menu')
+//     ];
+
+//     var el1 = list1[num];
+//     var el2 = list2[num];
+
+//     if (el2.style.display == "block") {
+//         el1.setAttribute('src', 'image/down.png');
+//         el2.style.display = "none";
+//     }
+//     else {
+//         el2.style.display = "block";
+//         el1.setAttribute('src', 'image/up.png');
+//     }
+// }
 
 
-    var el1 = list1[num];
-    var el2 = list2[num];
 
-    if (el2.style.display == "block") {
-        el1.setAttribute('src', 'image/down.png');
-        el2.style.display = "none";
-    }
-    else {
-        el2.style.display = "block";
-        el1.setAttribute('src', 'image/up.png');
-    }
-}
+
+$('#all').hover( function(){
+    $('#all_menu').toggleClass('active');
+})
+
+$('#new').hover( function(){
+    $('#new_menu').toggleClass('active');
+})
+
+$('#see').hover( function(){
+    $('#see_menu').toggleClass('active');
+})
+
+$('#symku').hover( function(){
+    $('#symku_menu').toggleClass('active');
+})
+
+$('#odayg').hover( function(){
+    $('#odayg_menu').toggleClass('active');
+})
+
+$('#sezon').hover( function(){
+    $('#sezon_menu').toggleClass('active');
+})
